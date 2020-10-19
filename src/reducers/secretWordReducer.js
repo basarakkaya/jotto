19ae2645601c627c9bbd47b23421721226a1d247
Osman Başar Akkaya <osmanbasar.akkaya@tokeninc.com>
2020-10-19 17:36:04 +0300

    Secret word reducer and moxios tests

--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,9 +1,16 @@
+import { actionTypes } from "../actions";
+
 /**
  * @function secretWordsReducer
- * @param {string} state Secret word
+ * @param {string} state State before reducer
  * @param {object} action action to be reduced
  * @returns {string} new secretWord state
  */
-export default (state = "party", action) => {
-  return state;
+export default (state = null, action) => {
+  switch (action.type) {
+    case actionTypes.SET_SECRET_WORD:
+      return action.payload;
+    default:
+      return state;
+  }
 };
