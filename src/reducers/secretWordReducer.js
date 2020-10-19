dd0c90f4b6d4994f4e9590fa268e19a8f22dff78
Osman Başar Akkaya <osmanbasar.akkaya@tokeninc.com>
2020-10-19 16:20:43 +0300

    Completed integration test for guessedWords

---
+++ src/reducers/secretWordReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function secretWordsReducer
+ * @param {string} state Secret word
+ * @param {object} action action to be reduced
+ * @returns {string} new secretWord state
+ */
+export default (state = "party", action) => {
+  return state;
+};
